c78348e59bb8b28450c6330b98cc7b55e2cb010b
yuuki.kato <0me3210g039530z@gmail.com>
2022-05-06 09:14:43 +0900

    modify list_3-1

--- section_3/list_3-1.py
+++ section_3/list_3-1.py
@@ -16,6 +16,11 @@ x = [1,2,3,4,5,6,7]
 y = [64.3, 63.8, 63.6, 64.0, 63.5, 63.2, 63.1]
 
 # グラフを描画
-plt.plot(x, y) #折れ線を描画
-plt.grid(color='0.8') # グリッドを表示
+
+# plt.plot(x, y) #折れ線を描画
+#plt.plot(x, y, marker='o') # 折線に点を描画したかったら
+# plt.plot(x, y, color='red') # 折線の色を指定
+plt.plot(x, y, color='red', marker='o') # 折線に点を描画, 色を指定
+
+plt.grid(color='0.8') # グリッドを表示, 数値一つだけでモノクロ濃度指定となる。
 plt.show() # 画面に表示
