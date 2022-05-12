6939458bb342b5003ff71c301d3fcaeebd813432
yuuki.kato <0me3210g039530z@gmail.com>
2022-05-13 08:29:36 +0900

    add comment list3-7

--- section_3/list_3-7.py
+++ section_3/list_3-7.py
@@ -1,12 +1,24 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# 角度
+# 角度 0〜359の角度を配列で取得
 th = np.arange(0, 360)
 
-# 円周上の点Pの座標
+# 円周上の点Pの座標、radians関数は弧度法の値に変換する処理
 x = np.cos(np.radians(th))
 y = np.sin(np.radians(th))
+"""
+点(2,3)を中心にした円にしたい場合
+x = np.cos(np.radians(th)) + 2
+y = np.sin(np.radians(th)) + 3
+"""
+
+"""
+# 半径が１以外の円を描画する場合
+r = 5 # 円の半径
+x = r * np.cos(np.radians(th))
+y = r * np.sin(np.radians(th))
+"""
 
 # 描画
 plt.plot(x, y)
